dafe84e1ee09859fd6880b97841c57f0b3b01135
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-09-05 04:19:00 +0800

    Update and rename main.py to Day8/main.py

--- Day8/main.py
+++ Day8/main.py
@@ -7,12 +7,7 @@ def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
     shift_amount *= -1
-  for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    
+  for char in start_text:    
     if not char.isalpha():
       end_text += char
     else:
@@ -21,14 +16,6 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text += alphabet[new_position]
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
-
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-
-
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 end_of_codes = False  
 while not end_of_codes:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -42,11 +29,7 @@ while not end_of_codes:
   if cont_caesar != "yes":
     end_of_codes = True
   
-
   if shift > len(alphabet):
     shift %2 != 0
      
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-#Hint: Think about how you can use the modulus (%).
+
