49f74b7a8fb43b19905a9902f93b34ca039c9767
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-10-05 03:14:51 +0800

    Create main.py

---
+++ Day18/Spirograph Exercise/main.py
@@ -0,0 +1,19 @@
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+tim.shape("turtle")
+
+colors = ["LightSteelBlue", "CornflowerBlue", "Thistle", "MediumPurple", "MediumSlateBlue"]
+
+tim.speed('fastest')
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.color(random.choice(colors))
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
