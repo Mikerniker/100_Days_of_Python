8fa24bf81f41b25aece70f8703033270b2cf7443
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-10-15 20:14:05 +0800

    Update food.py

--- Day21/food.py
+++ Day21/food.py
@@ -10,9 +10,6 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
-        self.goto(random_x, random_y)
         self.refresh()
 
     def refresh(self):
