98d426ea6dfdf4256fb4e790e21c40ceea17517d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-10-24 02:57:42 +0800

    Create main.py

---
+++ Day25/States Game/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+from answer import Answer
+from scoring import Scoreboard
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+  
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+learn_state = []
+for state in all_states:
+    if state not in guessed_states:
+        learn_state.append(state)
+
+df = pandas.DataFrame(learn_state)
+df.to_csv("states_to_learn.csv")
+
