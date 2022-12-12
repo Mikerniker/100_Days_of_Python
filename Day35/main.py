3f103755003d76b8154ed12c852043dc859f9f86
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-13 05:42:46 +0800

    updated main added telegram

--- Day35/main.py
+++ Day35/main.py
@@ -1,7 +1,38 @@
 import requests
+import os
+
+
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+
+def send_telegram_message(message):
+    parameters = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+    }
+
+    response = requests.post(url=f"https://api.telegram.org/bot{TOKEN}/sendMessage", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+# Send a Telegram sticker (optional)
+def send_telegram_sticker():
+    new_parameters = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "sticker": "CAACAgIAAxkBAAMIY5eEkgzIWhbUHzVBwrEvoO1I4A4AAiYBAAKmREgLTZCtuf2gbdorBA",
+    }
+
+    response2 = requests.post(url=f"https://api.telegram.org/bot{TOKEN}/sendSticker", params=new_parameters)
+    response2.raise_for_status()
+    data2 = response2.json()
+    return data2
+
+# Open Weather Map API
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = "my_api_key"
+api_key = os.environ.get("OWM_API_KEY")
 
 weather_params = {
     "lat": 14.676041,
@@ -14,12 +45,14 @@ response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 
-list_of_twelve = weather_data["hourly"][0:12]
+list_of_twelve_hours = weather_data["hourly"][0:12]
 will_rain = False
-for i in list_of_twelve:
+for hour in list_of_twelve_hours:
     condition_code = i["weather"][0]["id"]
     if int(condition_code) < 700:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella.")
+    message = "It's going to rain, don't forget to bring an umbrella!"
+    send_telegram_message(message)
+    send_telegram_sticker()
