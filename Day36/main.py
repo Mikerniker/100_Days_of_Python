48af616d1d3f7f1acb2948b3e07f6fef6cdfb5a9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-14 05:02:46 +0800

    added day 35 main

---
+++ Day36/main.py
@@ -0,0 +1,29 @@
+
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+from datetime import datetime
+
+today = datetime.now()
+print(today.strftime("%Y%m%d"))
+
+import requests
+
+API = ""
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK,
+    "apikey": API
+}
+
+url = 'https://www.alphavantage.co/query'
+
+stock_data = requests.get(url, params=stock_params)
+stock_data.raise_for_status()
+data = stock_data.json()['Time Series (Daily)']
+# print(data)
