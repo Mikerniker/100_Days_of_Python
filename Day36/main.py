5d17652002e07f77f7d9777e48e74fa50fab1bc0
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-17 19:42:21 +0800

    updated functions

--- Day36/main.py
+++ Day36/main.py
@@ -4,12 +4,15 @@ from datetime import datetime
 
 TELEGRAM_CHAT_ID = "insert id"
 TELEGRAM_TOKEN = "add your token"
-ALPHAVANTAGE_API = ""
+
+ALPHAVANTAGE_API = "add your api"
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-NEWS_API = ""  #insert api
+
+NEWS_API = "add your api" 
 alphavantage_url = 'https://www.alphavantage.co/query'
 news_url = 'https://newsapi.org/v2/everything'
+
 
 def send_telegram_message(message):
     parameters = {
@@ -23,38 +26,52 @@ def send_telegram_message(message):
     return data
 
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# STEP 1: Get stocks
 
+def get_stocks():
+    stock_params = {
+        "function": "TIME_SERIES_DAILY_ADJUSTED",
+        "symbol": STOCK,
+        "apikey": ALPHAVANTAGE_API,
+    }
+
+    stock_data = requests.get(alphavantage_url, params=stock_params)
+    stock_data.raise_for_status()
+    stocks = stock_data.json()['Time Series (Daily)']
+    return stocks
+
+# STEP 2 Get news.
+
+def get_news():
+    news_params = {
+        "q": COMPANY_NAME,
+        "from": yesterday_formatted,
+        "to": day_before_yesterday_formatted,
+        "language": "en",
+        "sortBy": "relevancy",
+        "pageSize": 3,
+        "apiKey": NEWS_API,
+    }
+    request = requests.get(news_url, params=news_params)
+    request.raise_for_status()
+    news_data = request.json()['articles']
+
+    return news_data
+
+
+# Get date range
 
 today = datetime.now()
-# print(today.strftime("%Y-%m-%d"))
+
 yesterday = (int(today.strftime("%d")))- 1
 yesterday_formatted = today.strftime(f"%Y-%m-{yesterday}")
-# print(yesterday_formatted)
 
 day_before_yesterday = (int(today.strftime("%d")))- 2
 day_before_yesterday_formatted = today.strftime(f"%Y-%m-{day_before_yesterday}")
-# print(day_before_yesterday_formatted)
 
+stock_yesterday = get_stocks()[yesterday_formatted]["4. close"]
+stock_day_before_yesterday = get_stocks()[day_before_yesterday_formatted]["4. close"]
 
-
-stock_params = {
-    "function": "TIME_SERIES_DAILY_ADJUSTED",
-    "symbol": STOCK,
-    "apikey": ALPHAVANTAGE_API,
-}
-
-
-stock_data = requests.get(alphavantage_url, params=stock_params)
-stock_data.raise_for_status()
-data = stock_data.json()['Time Series (Daily)']
-print(data)
-
-stock_yesterday = data[yesterday_formatted]["4. close"]
-stock_day_before_yesterday = data[day_before_yesterday_formatted]["4. close"]
-print(stock_yesterday)
-print(stock_day_before_yesterday)
 
 price_difference = float(stock_yesterday) - float(stock_day_before_yesterday)
 print(f"Price difference: {price_difference}")
@@ -62,23 +79,6 @@ percent_diff = abs(price_difference / float(stock_yesterday) * 100)
 print("Percent:" + str(percent_diff))
 
 
-# STEP 2 GET NEWS: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-
-# def get_news():
-news_params = {
-    "q": COMPANY_NAME,
-    "from": yesterday_formatted,
-    "to": day_before_yesterday_formatted,
-    "language": "en",
-    "sortBy": "relevancy",
-    "pageSize": 3,
-    "apiKey": NEWS_API,
-}
-request = requests.get(news_url, params=news_params)
-request.raise_for_status()
-news_data = request.json()['articles']
 
 
 # def stock_changed():
