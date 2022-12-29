8a74d76499362b05a6a27a406431ddcd5f740ddb
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-30 05:40:33 +0800

    updated data manager function

--- Day39/data_manager.py
+++ Day39/data_manager.py
@@ -2,12 +2,13 @@ import requests
 
 class DataManager:
 
-    def add_iatacode_to_sheet(self, url, data):
-        for item in data:
-            new_url = f"{url}/{item['id']}"
+    def add_iatacode_to_sheet(self, url, sheety):
+        """This function adds the iataCode to the Google sheet."""
+        for city in sheety:
+            new_url = f"{url}/{city['id']}"
             flight_codes = {
                 "sheet1": {
-                    "iataCode": "TESTING",
+                    "iataCode": city['iataCode'],
                 }
             }
             edit_sheety_row = requests.put(url=new_url, json=flight_codes)
