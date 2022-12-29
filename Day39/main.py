b235a07596d2d4380ae50dd7bd644303408b41db
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-30 05:18:04 +0800

    updated flightsearch call in main

--- Day39/main.py
+++ Day39/main.py
@@ -5,7 +5,7 @@ from data_manager import DataManager
 
 #Get sheety data
 
-SHEETY_URL = '#insertsheetyurl'
+SHEETY_URL = 'https://api.sheety.co/#insertsheetyurl'
 
 sheety_table = requests.get(SHEETY_URL)
 
@@ -17,10 +17,15 @@ all_prices = [item['lowestPrice'] for item in sheet_data]
 print(all_prices)
 
 #Search for flights
-flights = FlightSearch()
-pprint(flights.add_iata_code(sheet_data))
+# flights = FlightSearch()
+# pprint(flights.add_iata_code(sheet_data))
 
 
 #Add to sheety spreadsheet
-managed_data = DataManager()
-print(managed_data.add_iatacode_to_sheet(SHEETY_URL, sheet_data).text)
+# managed_data = DataManager()
+# print(managed_data.add_iatacode_to_sheet(SHEETY_URL, sheet_data).text)
+
+#Update iata codes in sheet data
+flights = FlightSearch()
+sheet_data = flights.search_iata_code(sheet_data)
+pprint(sheet_data)
