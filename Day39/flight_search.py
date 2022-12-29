1ce0359a6d81753ed3c9ba70a34b26f1beb0b026
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-30 05:10:05 +0800

    updated flight search functions

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -14,19 +14,29 @@ class FlightSearch:
     #     return data
 
 #TESTING CODE:
-    def add_iata_code(self, data):
-        cities = ["Paris", "Berlin", "Tokyo", "Sydney", "Istanbul", "Kuala Lumpur", "New York",
-                  "San Francisco", "Cape Town"]
-        for city in cities:
-            tequila_parameters = {
-                "term": city,
+    def call_tequila(self, city):
+        """This function makes a request to Kiwi Partners Tequila API to get the city iata code"""
+
+        tequila_parameters = {
+            "term": city
             }
 
-            headers = {
-                "apikey": TEQ_API_KEY,
+        headers = {
+            "apikey": TEQ_API_KEY,
             }
 
-            iata_codes = requests.get(TEQ_QUERY_ENDPOINT, params=tequila_parameters, headers=headers)
-            iata_codes.raise_for_status()
-        data = iata_codes.json()
+        iata_codes = requests.get(TEQ_QUERY_ENDPOINT, params=tequila_parameters, headers=headers)
+        iata_codes.raise_for_status()
+        data = iata_codes.json()['locations'][0]['code']
         return data
+
+    def search_iata_code(self, sheety):
+        """
+        This function checks the sheety data to see if a city has an iata code
+        and updates it by calling Tequila and adding the iata to sheety.
+        """
+        for city_name in sheety:
+            if city_name['iataCode'] == '':
+                iata = self.call_tequila(city_name['city'])
+                city_name['iataCode'] = iata
+        return sheety
