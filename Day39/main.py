7bc8aa9d4834cdc93cb76546a3400fa1867ba85d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-01 06:14:21 +0800

    deleted searchcheap flights code from main

--- Day39/main.py
+++ Day39/main.py
@@ -35,48 +35,6 @@ managed_data = DataManager()
 managed_data.add_iatacode_to_sheet(SHEETY_URL, sheet_data)
 
 
-#TESTING STEP 4:
+#GET CHEAP FLIGHTS
+print(flights.search_cheap_flights(sheet_data))
 
-from datetime import datetime, timedelta
-
-today = datetime.now()
-
-print(today.strftime("%d/%m/%Y"))
-
-tomorrow = today + timedelta(1)
-print(tomorrow.strftime("%d/%m/%Y"))
-tomorrow_formatted = tomorrow.strftime("%d/%m/%Y")
-
-six_months = tomorrow + timedelta(days=180)
-print(type(six_months.strftime("%d/%m/%Y")))
-six_months_formatted = six_months.strftime("%d/%m/%Y")
-
-#STEP 4.1
-TEQ_SEARCH_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-TEQ_API_KEY = "add api key"
-
-# cities = ["PAR", "BER", "TYO", "SYD", "IST", "KUL", "NYC", "SFO", "CPT"]
-#
-# for city in cities:
-tequila_newparameters = {
-    "fly_from": "LON",
-    "fly_to":  "PAR",
-    "date_from": tomorrow_formatted,  #tomorrow, "01/01/2023"
-    "date_to": six_months_formatted, #30/06/2023", #six_months,
-    "nights_in_dst_from": 7,
-    "nights_in_dst_to": 28,
-    "flight_type": "round",
-    "one_for_city": 1,
-    "curr": "GBP",
-    "max_stopovers": 0,
-}
-
-headers = {
-    "apikey": TEQ_API_KEY,
-    "Content-Type": "application/json",
-}
-
-flight_check = requests.get(TEQ_SEARCH_ENDPOINT, params=tequila_newparameters, headers=headers)
-flight_check.raise_for_status()
-data = flight_check.json()
-print(data)
