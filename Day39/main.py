e89f0d34f68b882638f6f290d015c9debffea671
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-02 06:49:43 +0800

    updated main code

--- Day39/main.py
+++ Day39/main.py
@@ -2,6 +2,11 @@ import requests
 from pprint import pprint
 from flight_search import FlightSearch
 from data_manager import DataManager
+from notification_manager import NotificationManager
+
+flights = FlightSearch()
+managed_data = DataManager()
+notify = NotificationManager()
 
 #Get sheety data
 
@@ -13,28 +18,31 @@ sheety_table.raise_for_status()
 sheet_data = sheety_table.json()['sheet1']
 pprint(sheet_data)
 
-all_prices = [item['lowestPrice'] for item in sheet_data]
-print(all_prices)
-
-#Search for flights
-# flights = FlightSearch()
-# pprint(flights.add_iata_code(sheet_data))
-
-
-#Add to sheety spreadsheet
-# managed_data = DataManager()
-# print(managed_data.add_iatacode_to_sheet(SHEETY_URL, sheet_data).text)
-
-#Update iata codes in sheet data
-flights = FlightSearch()
+# Look for IATA Codes
 sheet_data = flights.search_iata_code(sheet_data)
-pprint(sheet_data)
+# pprint(sheet_data)
 
-#Add changes to Sheety
-managed_data = DataManager()
+# Put IATA Codes in Sheety
 managed_data.add_iatacode_to_sheet(SHEETY_URL, sheet_data)
 
+# Search for Cheap Flights and send notification
 
-#GET CHEAP FLIGHTS
-print(flights.search_cheap_flights(sheet_data))
+DEPART_FROM = "LON"
+
+for item in sheet_data:
+    available_flights = flights.search_cheap_flights(DEPART_FROM, item['iataCode'])
+    if item['lowestPrice'] > available_flights.price:
+        alert = f"Low price alert! Only Gbp {available_flights.price} to fly from " \
+                f"{available_flights.departure_city_name}-" \
+                f"{available_flights.arrival_city_name} " \
+                f"to {available_flights.departure_airport_iata_code}-" \
+                f"{available_flights.arrival_airport_iata_code}, " \
+                f"from {available_flights.outbound_date} to " \
+                f"{available_flights.inbound_date}"
+        notify.send_telegram_message(alert)
+        print(alert)
+    else:
+        print(f"A flight to {available_flights.arrival_city_name} is {available_flights.price}. That's too expensive")
+
+
 
