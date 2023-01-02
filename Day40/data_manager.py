4c6d37f66eae84cbd548ea6e156c740297ca20c1
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-03 04:16:02 +0800

    updated data manager

---
+++ Day40/data_manager.py
@@ -0,0 +1,15 @@
+import requests
+
+class DataManager:
+
+    def add_iatacode_to_sheet(self, url, sheety):
+        """This function adds the iataCode to the Google sheet."""
+        for city in sheety:
+            new_url = f"{url}/{city['id']}"
+            flight_codes = {
+                "sheet1": {
+                    "iataCode": city['iataCode'],
+                }
+            }
+            edit_sheety_row = requests.put(url=new_url, json=flight_codes)
+        return edit_sheety_row
