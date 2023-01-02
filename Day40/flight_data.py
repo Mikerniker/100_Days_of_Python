824467ad6e742ce0dba8a35b29b95f28111f56f5
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-03 04:17:32 +0800

    updated flight data

---
+++ Day40/flight_data.py
@@ -0,0 +1,15 @@
+class FlightData:
+    #This class is responsible for structuring the flight data.
+    def __init__(self, price, departure_city_name, departure_airport_iata_code,
+                 arrival_city_name, arrival_airport_iata_code, outbound_date,
+                 inbound_date, stop_overs=0, via_city=""):
+        self.price = price
+        self.departure_city_name = departure_city_name
+        self.departure_airport_iata_code = departure_airport_iata_code
+        self.arrival_city_name = arrival_city_name
+        self.arrival_airport_iata_code = arrival_airport_iata_code
+        self.outbound_date = outbound_date
+        self.inbound_date = inbound_date
+        self.stop_overs = stop_overs
+        self.via_city = via_city
+
