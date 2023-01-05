3a2d8a82a7b73a18fb86267b828324ff379d1903
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-06 05:42:53 +0800

    modified flight data with stopover and via city

--- Day40/flight_data.py
+++ Day40/flight_data.py
@@ -2,7 +2,7 @@ class FlightData:
     #This class is responsible for structuring the flight data.
     def __init__(self, price, departure_city_name, departure_airport_iata_code,
                  arrival_city_name, arrival_airport_iata_code, outbound_date,
-                 inbound_date, stop_overs=0, via_city=""):
+                 inbound_date, via_city):
         self.price = price
         self.departure_city_name = departure_city_name
         self.departure_airport_iata_code = departure_airport_iata_code
@@ -10,6 +10,6 @@ class FlightData:
         self.arrival_airport_iata_code = arrival_airport_iata_code
         self.outbound_date = outbound_date
         self.inbound_date = inbound_date
-        self.stop_overs = stop_overs
+        self.stop_overs = 0
         self.via_city = via_city
 
