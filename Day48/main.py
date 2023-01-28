59e32af7716a79f42e132c6a19d683e337f59803
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-29 05:43:49 +0800

    added practice code which will be revised later

--- Day48/main.py
+++ Day48/main.py
@@ -1,1 +1,33 @@
-#TEST
+#TESTING;  NOT THE FINAL CODE
+
+
+from selenium import webdriver
+
+chrome_driver_path = "D:\DEVELOPMENT\chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.python.org/")
+
+
+find_the_thing = driver.find_elements_by_css_selector(".event-widget ul.menu time")
+
+dates = []
+for i in find_the_thing:
+    dates.append(i.text)
+
+print(dates)
+
+event_names = driver.find_elements_by_css_selector(".event-widget ul.menu a")
+events =[]
+for a in event_names:
+    events.append(a.text)
+print(events)
+
+all_things = driver.find_elements_by_css_selector(".event-widget ul.menu")
+
+for all in all_things:
+    print(all.text)
+
+
+driver.quit()
