ea762ae8025d96368d6d8cc8b78f8e3ad495c4a2
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-01-30 05:26:51 +0800

    updated events and dates codes

--- Day48/main.py
+++ Day48/main.py
@@ -11,24 +11,21 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.python.org/")
 
 
-find_the_thing = driver.find_elements_by_css_selector(".event-widget ul.menu time")
+get_dates = driver.find_elements_by_css_selector(".event-widget ul.menu time")
 
 dates = []
-for i in find_the_thing:
-    dates.append(i.text)
+for date in get_dates:
+    dates.append(date.text)
 
 print(dates)
 
-event_names = driver.find_elements_by_css_selector(".event-widget ul.menu a")
+get_events = driver.find_elements_by_css_selector(".event-widget ul.menu a")
 events =[]
-for a in event_names:
-    events.append(a.text)
+for event in get_events:
+    events.append(event.text)
 print(events)
 
-all_things = driver.find_elements_by_css_selector(".event-widget ul.menu")
 
-for all in all_things:
-    print(all.text)
 
 
 driver.quit()
