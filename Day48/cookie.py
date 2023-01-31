01e8186a122881dbfbd72d9f17074b8df79de42f
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-02-01 05:07:46 +0800

    added cookie money and cursor price

--- Day48/cookie.py
+++ Day48/cookie.py
@@ -11,3 +11,13 @@ countdown = 50
 while countdown > 0:
     cookie.click()
     countdown -= 1
+
+cookie_money = driver.find_element_by_id("money")
+print(cookie_money.text)
+print(type(cookie_money.text))
+
+
+cursor_price = driver.find_element_by_css_selector("#buyCursor b")
+cursor_words = cursor_price.text
+price = int(cursor_words.split(" ")[2])
+print(price)
