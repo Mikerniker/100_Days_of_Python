c9a4eadb9ed15dcf745a797c43ee120c1d59d0ce
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-02-03 05:12:27 +0800

    modified get price function

--- Day48/cookie.py
+++ Day48/cookie.py
@@ -2,6 +2,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+
 
 chrome_driver_path = "D:\DEVELOPMENT\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -25,31 +27,10 @@ price = int(cursor_words.split(" ")[2])
 print(price)
 
 #TEST
+
 def get_price(selector):
-    try:
-        price = driver.find_element_by_css_selector(f"#{selector}")
-        words = price.text
-        cookie_number = words.split(" ")[2]
-        print(f"{selector}: {cookie_number}")
-        return cookie_number
-    except selenium.common.exceptions.NoSuchElementException:
-        price = driver.find_element_by_id(selector)
-        words = price.text
-        cookie_number = words.split(" ")[2]
-        print(f"{selector}: {cookie_number}")
-        return cookie_number
-    except ValueError:
-        print(f"{selector} not available")
-    except IndexError:
-        print("No amount available")
-
-
-
-# print(get_price("buyAlchemy lab"))
-
-all_ids = ["buyGrandma", "buyFactory", "buyMine", "buyShipment",
-           "buyAlchemy lab", "buyPortal", "buyTime machine", "buyElder Pledge"]
-
-for id in all_ids:
-    print(get_price(id))
-
+    price = driver.find_element_by_css_selector(f"#{selector}")
+    words = price.text
+    cookie_number = words.split(" ")[2].split("\n")[0]
+    # print(f"{selector}: {cookie_number}")
+    return cookie_number
