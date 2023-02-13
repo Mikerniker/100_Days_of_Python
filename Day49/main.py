2826e5939de52584e7c6e604c5f93c8a8716d57b
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-02-14 04:28:49 +0800

    added code to find and sign in with username and password

--- Day49/main.py
+++ Day49/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+MY_EMAIL = "********************"
+MY_PASSWORD = "*****************"
+
 chrome_driver_path = "D:\DEVELOPMENT\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3362855988&f_AL=true&f_WT=2&geoId=103121230&keywords=python%20developer&location=Philippines&refresh=true")
@@ -10,3 +13,13 @@ sign_in_webpage.click()
 
 linkedin_signin_window = driver.window_handles[0]
 print(linkedin_signin_window)
+
+linkedin = driver.switch_to.window(linkedin_signin_window)
+username = driver.find_element_by_id("username")
+username.send_keys(MY_EMAIL)
+
+password = driver.find_element_by_id("password")
+password.send_keys(MY_PASSWORD)
+
+signin_button = driver.find_element_by_class_name("btn__primary--large")
+signin_button.click()
