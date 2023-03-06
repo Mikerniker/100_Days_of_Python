ea58a5c14ac634d1927bb6792401ff25fb857a84
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-03-07 05:38:11 +0800

    added code to maximize window

--- Day49/main.py
+++ Day49/main.py
@@ -9,6 +9,7 @@ MY_PASSWORD = "*****************"
 chrome_driver_path = "D:\DEVELOPMENT\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3362855988&f_AL=true&f_WT=2&geoId=103121230&keywords=python%20developer&location=Philippines&refresh=true")
+driver.maximize_window()
 
 sign_in_webpage = driver.find_element_by_link_text("Sign in")
 sign_in_webpage.click()
