51557e61017a5289899f098202c2c718485f568e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-03-20 05:49:14 +0800

    added login with username and password to okcupid

--- Day50/ok_cupid.py
+++ Day50/ok_cupid.py
@@ -14,3 +14,21 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.okcupid.com/")
 wait = WebDriverWait(driver, 10)
 driver.maximize_window()
+
+# SIGN INTO OK CUPID
+time.sleep(3)
+sign_in = driver.find_element(By.CLASS_NAME, "c0J0grIjyKY6YuiL9OO7")
+sign_in.click()
+
+time.sleep(10)
+
+username = driver.find_element(By.NAME, "username")
+username.send_keys(MY_EMAIL)
+
+password = driver.find_element(By.NAME, "password")
+password.send_keys(MY_PASSWORD)
+
+next_button = driver.find_element(By.CLASS_NAME, "login-actions-button")
+next_button.click()
+
+time.sleep(50)
