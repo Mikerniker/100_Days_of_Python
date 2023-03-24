40932db8f9387f05cf521b168ea349aca9af0af8
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-03-25 06:04:49 +0800

    added results for upload and download speeds

--- Day51/internet_speed.py
+++ Day51/internet_speed.py
@@ -12,3 +12,12 @@ driver.get("https://www.speedtest.net/")
 
 go_button = driver.find_element(By.CLASS_NAME, "js-start-test")
 go_button.click()
+
+time.sleep(120)
+
+results = driver.find_elements(By.CLASS_NAME, "result-data")
+for result in results:
+    print(result.text)
+
+print(f"download: {results[1].text}")
+print(f"upload: {results[2].text}")
