74e153052cadd23dd54c36c26de7775e47af9a39
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-04-05 04:44:17 +0800

    removed the undetetected chromedriver

--- Day51/main2.py
+++ Day51/main2.py
@@ -1,6 +1,4 @@
-#Replace webdriver with this:
-import undetected_chromedriver as uc
-# from selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException 
