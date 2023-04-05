4860c2dda2f4aaa913d2e7f9b85e0c8d507b0d88
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-04-06 05:05:56 +0800

    added a class to day 52 main

--- Day52/main.py
+++ Day52/main.py
@@ -5,3 +5,16 @@ CHROME_DRIVER_PATH = ""
 SIMILAR_ACCOUNT = ""
 USERNAME = ""
 PASSWORD = ""
+
+class Instafollower:
+    def __init__(self) -> None:
+        pass
+
+    def login(self):
+        pass
+    
+    def find_followers(self):
+        pass
+    
+    def follow(self):
+        pass
