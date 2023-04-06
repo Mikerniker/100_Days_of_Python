05bab09211e2fb0a51fa28280a9401aefbc2b08a
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-04-07 05:39:32 +0800

    added init to class to search web driver

--- Day52/main.py
+++ Day52/main.py
@@ -2,18 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 # from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys #ADDED
+from selenium.webdriver.common.keys import Keys 
 import time
 
 
-CHROME_DRIVER_PATH = ""
-SIMILAR_ACCOUNT = ""
+CHROME_DRIVER_PATH = "D:\DEVELOPMENT\chromedriver.exe"
+SIMILAR_ACCOUNT = "selfcareisforeveryone"
 USERNAME = ""
 PASSWORD = ""
 
 class Instafollower:
     def __init__(self) -> None:
-        pass
+        self.service = Service(executable_path=CHROME_DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.service)
+
 
     def login(self):
         pass
