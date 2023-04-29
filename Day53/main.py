9e4eec700dd4a7ea3ad852577b016dd0b31cd02e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-04-30 03:15:16 +0800

    finding input in forms

--- Day53/main.py
+++ Day53/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
 #requests allows us to get hold of the data from a particular url
 zillow = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.64481581640625%2C%22east%22%3A-122.22184218359375%2C%22south%22%3A37.64220115428586%2C%22north%22%3A37.908142595089714%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
@@ -53,4 +54,12 @@ service = Service(executable_path=CHROME_DRIVER_PATH)
 driver = webdriver.Chrome(service=service)
 driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdgf9wgrDe1E_GtfgL1NdfM-VQhL6bw1Md_qS3-T3lxELCLSA/viewform?usp=sf_link")
 
-all_fields = driver.find_elements(By.NAME, "input")
+for item in range(2):
+    driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdgf9wgrDe1E_GtfgL1NdfM-VQhL6bw1Md_qS3-T3lxELCLSA/viewform?usp=sf_link")
+
+    time.sleep(5)
+    all_fields = driver.find_elements(By.CLASS_NAME, "whsOnd.zHQkBf")
+    # all_fields = driver.find_element(By.XPATH, "/html/body/div/div[3]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    # print(all_fields)
+    for item in all_fields:
+        item.send_keys("attempt1")
