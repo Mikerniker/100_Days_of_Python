dd8c9a7068760be5e9698fb4b8bc760720e88db0
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-05-04 05:39:05 +0800

    added decorators to the functions

--- Day54/main.py
+++ Day54/main.py
@@ -5,8 +5,14 @@ import time
 current_time = time.time()
 print(current_time)
 
-def speed_calc_decorator():
-    pass
+def speed_calc_decorator(function):
+    def run_speed():
+       start_time = time.time()
+       function
+       end_time = current_time
+       difference = start_time - end_time
+       print(f"{function.__name__} run speed: {difference}")
+    return run_speed
 
 def fast_function():
     for i in range(10000000):
