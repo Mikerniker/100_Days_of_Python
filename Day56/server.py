615e140c14b5b452f34fb7fb44f42ff35c4a9dec
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-05-10 03:37:34 +0800

    added day 56

--- Day56/server.py
+++ Day56/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('my_cv.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
