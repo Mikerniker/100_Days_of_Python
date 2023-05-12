344a8422ae938b82a61086ca54b3205ca23e202e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-05-13 05:49:28 +0800

    updated check name function

--- Day57/challenge1/server.py
+++ Day57/challenge1/server.py
@@ -10,15 +10,16 @@ def home():
 
 @app.route("/guess/<name>")
 def check_name(name):
-    response = requests.get(url=f"https://api.agify.io?name={name}")
-    response.raise_for_status()
-    data = response.json()
-    age_of_name = data["age"]
+    def age_gender(url_to_check):
+        response = requests.get(url=url_to_check)
+        response.raise_for_status()
+        data = response.json()
+        return data
 
-    response2 = requests.get(url=f"https://api.genderize.io?name={name}")
-    response2.raise_for_status()
-    data2 = response2.json()
-    gender_of_name = data2["gender"]
+    genderize_url = f"https://api.genderize.io?name={name}"
+    agify_url = f"https://api.agify.io?name={name}"
+    gender_of_name = age_gender(genderize_url)["gender"]
+    age_of_name = age_gender(agify_url)["age"]
 
     return render_template("names.html", name=name.title(), age=age_of_name, gender=gender_of_name)
 
