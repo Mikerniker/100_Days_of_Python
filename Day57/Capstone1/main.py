7a5625dc9c8f11406db33c1eca65aac2230b0b88
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-05-15 04:40:16 +0800

    updated main of day 57

--- Day57/Capstone1/main.py
+++ Day57/Capstone1/main.py
@@ -10,13 +10,16 @@ def home():
     all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
-# @app.route('/post/<blog_num>')
-# def post(blog_num):
-#     blog_url = "https://api.npoint.io/188d478a1aceaa6a6e0f"
-#     response = requests.get(blog_url)
-#     all_posts = response.json()
-#     return render_template("post.html", posts=all_posts)
-
+@app.route('/post/<blog_id>')
+def blog_post(blog_id):
+    blog_url = "https://api.npoint.io/188d478a1aceaa6a6e0f"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    post = all_posts[int(blog_id) - 1]
+    title = post["title"]
+    subtitle = post["subtitle"]
+    body = post["body"]
+    return render_template("post.html", title=title, subtitle=subtitle, body=body)
 
 if __name__ == "__main__":
     app.run(debug=True)
