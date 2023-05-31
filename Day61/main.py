06509179b0378efa608834c91d3e7fb296735e45
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-06-01 03:01:53 +0800

    added main code for flask

---
+++ Day61/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "mysecretstring"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
