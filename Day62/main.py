a4d7e6343d9a8ff077672aff2668a3db8ce5cb91
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-06-04 05:18:09 +0800

    added cafe route to main

--- Day62/main.py
+++ Day62/main.py
@@ -31,7 +31,22 @@ def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         print("True")
-    # Exercise:
+    # Exercis
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
     return render_template('add.html', form=form)
+
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding="utf8") as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
