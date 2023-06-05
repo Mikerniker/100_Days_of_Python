95207e681a083e7428fd48d60c2788883e1862ae
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-06-06 03:06:34 +0800

    updated add cafe route

--- Day62/main.py
+++ Day62/main.py
@@ -30,7 +30,6 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        # print("True")
         new_data = f"\n{form.cafe.data}, {form.location.data}, {form.opening_time.data}," \
                    f" {form.closing_time.data}, {form.coffee_rating.data}, {form.wifi_rating.data}, " \
                    f"{form.outlet_rating.data}"
