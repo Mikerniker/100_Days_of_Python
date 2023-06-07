5fb4797349a53f7629ad186f33d21a83c588d142
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-06-08 03:32:42 +0800

    added add function to main

--- Day63/main.py
+++ Day63/main.py
@@ -8,3 +8,20 @@ all_books = []
 @app.route('/')
 def home():
     return render_template("index.html", book_list=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == 'POST':
+        book_to_add =  {
+            "title": request.form['title'],
+            "author": request.form['author'],
+            "rating": request.form['rating'],
+        }
+        all_books.append(book_to_add)
+        return redirect(url_for('home'))
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
