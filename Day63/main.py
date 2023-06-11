06e3846ad92bb44fc56073e356432058738565b1
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-06-12 05:56:35 +0800

    added book class in main for sql

--- Day63/main.py
+++ Day63/main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+db = SQLAlchemy(app)
 
-all_books = []
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+db.create_all()
 
 
 @app.route('/')
