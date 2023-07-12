4194c2a530d6bbe9c9cc4a1a7aa555899c559b17
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-07-13 01:28:00 +0800

    added to main day 68

---
+++ Day68/main.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
