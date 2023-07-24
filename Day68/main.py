0fd050a1a72805a616e04e238f800c838941f520
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-07-25 01:10:34 +0800

    added table class for user

--- Day68/main.py
+++ Day68/main.py
@@ -9,3 +9,12 @@ app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+##CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+#Line below only required once, when creating DB. 
+# db.create_all()
