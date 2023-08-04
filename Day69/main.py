39fd6dff07b259b0717eb2633a86c60b12a68cd9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-08-05 03:44:14 +0800

    added register function

--- Day69/main.py
+++ Day69/main.py
@@ -37,9 +37,26 @@ class BlogPost(db.Model):
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = "users" 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 # Line below only required once, when creating DB.
 # db.create_all()
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(name=form.name.data,
+                        email=form.email.data,
+                        password=generate_password_hash(form.password.data,
+                                                        method='pbkdf2:sha256',
+                                                        salt_length=8))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("get_all_posts")) 
+    return render_template("register.html", form=form)
+
