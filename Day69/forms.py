9ed45359da4eae44eec13b225e18ae74349382cb
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-08-06 04:32:21 +0800

    added wtf form for blog post

--- Day69/forms.py
+++ Day69/forms.py
@@ -2,3 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
