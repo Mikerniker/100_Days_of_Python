c8e3e7cf87b5527b63d711fba316f3422eba5c55
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-08-06 04:31:42 +0800

    added forms py

---
+++ Day69/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email
+from flask_ckeditor import CKEditorField
