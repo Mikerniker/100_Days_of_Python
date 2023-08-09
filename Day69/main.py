e84dc2f6e7eadaef768d660891252e7a5be11c2a
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-08-10 02:01:46 +0800

    modified Blogpost by adding link to relational db with User

--- Day69/main.py
+++ Day69/main.py
@@ -36,11 +36,17 @@ db.init_app(app)
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
+
+    # For relational db Create Foreign Key, "user.id" the user refers to the tablename of User.
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    
+    #Create reference to the User object, the "posts" refers to the posts property in the User class.
+    author = relationship("User", back_populates="posts")
+
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
 
