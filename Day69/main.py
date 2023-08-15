12149460e3b59e0e840f327a0c73f77e9da4711f
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-08-15 22:51:22 +0800

    added add new post function

--- Day69/main.py
+++ Day69/main.py
@@ -177,3 +177,23 @@ def admin_only(function):
         # Otherwise continue with the route function
         return function(*args, **kwargs)
     return decorated_function
+
+
+# Use a decorator so only an admin user can create a new post
+@app.route("/new-post", methods=["GET", "POST"])
+@admin_only
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=current_user,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
