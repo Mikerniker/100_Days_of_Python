0b7f2703534298689a038e3ba078484fe6efca36
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-09-28 01:22:42 +0800

    added function to update the board

--- Day84/main.py
+++ Day84/main.py
@@ -10,3 +10,11 @@ board_position = {
     "4": (1, 0), "5": (1, 1), "6": (1, 2),
     "7": (2, 0), "8": (2, 1), "9": (2, 2)
 }
+
+# Function to update the board and board_position
+def update_board(position, player):
+    row, col = board_position[position]
+    if board[row][col] == "_":
+        board[row][col] = player
+    else:
+        print("That's not available.")
