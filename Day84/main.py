4d9b8ceb042fb3ec42718a6b8c2c1475b2258f1f
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-09-28 01:22:08 +0800

    added dictionary to identify position of board

--- Day84/main.py
+++ Day84/main.py
@@ -4,3 +4,9 @@ board = [["_", "_", "_"] for i in range(3)]
 for i in board:
     print(i)
 
+# Dictionary to map position on the board
+board_position = {
+    "1": (0, 0), "2": (0, 1), "3": (0, 2),
+    "4": (1, 0), "5": (1, 1), "6": (1, 2),
+    "7": (2, 0), "8": (2, 1), "9": (2, 2)
+}
