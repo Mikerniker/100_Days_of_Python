b43dda7f8d94a7ca2d6a4335ff43d13a7cad1d2b
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-10-01 04:52:35 +0800

    Added check winner function

--- Day84/main.py
+++ Day84/main.py
@@ -18,3 +18,24 @@ def update_board(position, player):
         board[row][col] = player
     else:
         print("That's not available.")
+
+
+def check(list, player):
+    return all(i == player for i in list)
+
+
+def check_winner(player):
+    # all_rows = [board[i] for i in range(3)]
+    row1 = board[0]
+    row2 = board[1]
+    row3 = board[2]
+    column1 = [board[i][0] for i in range(3)]
+    column2 = [board[i][1] for i in range(3)]
+    column3 = [board[i][2] for i in range(3)]
+    diag_left_right = [board[i][i] for i in range(3)]
+    diag_right_left = [board[i][2 - i] for i in range(3)]
+    check_board = [row1, row2, row3, column1, column2, column3, diag_left_right, diag_right_left]
+    for item in check_board:
+        if check(item, player):
+            print(f"Player: {player} wins!")
+            return True
