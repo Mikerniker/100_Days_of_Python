6e5c050b624fdef8bf52288782a38830c2164af8
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-10-17 02:41:36 +0800

    modified canvas section day 85

--- Day85/main.py
+++ Day85/main.py
@@ -10,12 +10,16 @@ window.title("Mik's Watermark")
 window.minsize(width=800, height=500)
 window.config(padx=20, pady=20)
 
+
 #Canvas
 canvas = Canvas(width=650, height=450, highlightthickness=0)
 burger_img = PhotoImage(file="hamburger.png")
 canvas.create_image(325, 225, image=burger_img)
-canvas.create_text(325, 225, text="watermark test", fill="white", font=("Arial", 35, "bold")) #adds watermark
+canvas.create_text(325, 225, text="", fill="white", font=("Arial", 35)) #adds watermark
 canvas.grid(column=0, row=0, columnspan=2)
+
+
+
 
 #Label
 my_label = tkinter.Label(text="Add Image to watermark", font=("Arial", 24, "bold"))
