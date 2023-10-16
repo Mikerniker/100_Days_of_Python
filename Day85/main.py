a066a0c7d946befd55a41a49999dd1256880927d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-10-17 02:42:44 +0800

    added add watermark function for day 85

--- Day85/main.py
+++ Day85/main.py
@@ -15,11 +15,13 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=650, height=450, highlightthickness=0)
 burger_img = PhotoImage(file="hamburger.png")
 canvas.create_image(325, 225, image=burger_img)
-canvas.create_text(325, 225, text="", fill="white", font=("Arial", 35)) #adds watermark
+watermark_text = canvas.create_text(325, 225, text="", fill="white", font=("Arial", 35)) #adds watermark
 canvas.grid(column=0, row=0, columnspan=2)
 
 
-
+def add_watermark():
+    watermark = input.get()
+    canvas.itemconfig(watermark_text, text=watermark)
 
 #Label
 my_label = tkinter.Label(text="Add Image to watermark", font=("Arial", 24, "bold"))
