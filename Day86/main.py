c7f341df4cb55d146324e869b6ddaa48bf8a9489
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-10 05:34:31 +0800

    added global variables day 86

---
+++ Day86/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import filedialog, font, colorchooser, messagebox
+import pandas
+import random
+
+# Global Variables
+CANVAS_WIDTH = 200
+CANVAS_HEIGHT = 125
+BACKGROUND_COLOR = "#16425D"
