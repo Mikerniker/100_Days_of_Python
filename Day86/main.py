e7b442d7452c6ad5021edb907cab8f669d09e29c
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-10 05:35:11 +0800

    added initial tkinter window day86

--- Day86/main.py
+++ Day86/main.py
@@ -7,3 +7,10 @@ import random
 CANVAS_WIDTH = 200
 CANVAS_HEIGHT = 125
 BACKGROUND_COLOR = "#16425D"
+
+# Tkinter
+window = Tk()
+window.title("Mik's Speed Typing Test")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.mainloop()
