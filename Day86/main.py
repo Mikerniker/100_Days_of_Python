23cf046365e9f4e129dceb5bbe573fb667854484
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-13 04:44:42 +0800

    added canvas day 86

--- Day86/main.py
+++ Day86/main.py
@@ -13,4 +13,16 @@ window = Tk()
 window.title("Mik's Speed Typing Test")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+
+#Canvas
+canvas = Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT,
+                highlightthickness=0, bg=BACKGROUND_COLOR)
+# typewriter = PhotoImage(file="typewriter.png")
+header_text = canvas.create_text(120, 90,
+                                 text="Speed Typing Test", fill="#fff",
+                                 font=("Georgia", 20, "bold"))
+typwriter_background = canvas.create_image(CANVAS_WIDTH/2, CANVAS_HEIGHT/4,
+                                           image=typewriter)
+canvas.grid(row=0, column=0)
+
 window.mainloop()
