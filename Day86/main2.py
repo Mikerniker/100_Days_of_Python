80170e08256f2a0999528973b3e29a5617a640c6
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-01-21 05:40:48 +0800

    modified text_widget day 86

--- Day86/main2.py
+++ Day86/main2.py
@@ -63,9 +63,9 @@ window.title("Mik's Speed Typing Test")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Create a Text widget with three rows
-text_widget = Text(window, height=4, width=28,
-                   wrap='word', font=("Georgia", 20, "bold"),)
-text_widget.grid(column=0, row=2, rowspan=4, sticky="nsew")
+text_widget = Text(window, height=4.5, width=25,
+                   wrap='word', font=("Georgia", 25))
+text_widget.grid(column=0, row=3, rowspan=4, sticky="nsew")
 
 insert_words()
 
