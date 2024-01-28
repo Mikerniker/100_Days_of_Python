048466678397629650cabeef60c73438ca05e50e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-01-29 04:48:11 +0800

    added tracer to remove animated movement

--- Day87/main.py
+++ Day87/main.py
@@ -5,7 +5,7 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Breakout Game")
-
+screen.tracer(0)
 
 # Create paddle
 paddle = Turtle()
@@ -30,7 +30,10 @@ screen.listen()
 screen.onkey(go_right, "Right")
 screen.onkey(go_left, "Left")
 
+game_is_on  = True
 
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
