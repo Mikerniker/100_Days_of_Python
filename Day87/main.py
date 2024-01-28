32a03592d3a0917c71c460d393a03283533b3c86
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-01-29 04:43:06 +0800

    created initial paddle

--- Day87/main.py
+++ Day87/main.py
@@ -1,9 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Breakout Game")
+
+
+# Create paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=1, stretch_len=8)
+paddle.penup()
+paddle.goto(0, -250)
+
 
 screen.exitonclick()
