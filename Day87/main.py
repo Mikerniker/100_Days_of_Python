3cf6c1e68733f14009b61d54290e7938c0423a42
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-01-30 00:29:10 +0800

    imported paddle and fixed call to paddle

--- Day87/main.py
+++ Day87/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 
@@ -7,28 +8,15 @@ screen.setup(width=800, height=600)
 screen.title("Breakout Game")
 screen.tracer(0)
 
-# Create paddle
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=1, stretch_len=8)
-paddle.penup()
-paddle.goto(0, -250)
+paddle = Paddle((0, -250))
 
-
-# Commands to move paddle
-def go_right():
-    new_x = paddle.xcor() + 20
-    paddle.goto(new_x, paddle.ycor())
-
-def go_left():
-    new_x = paddle.xcor() - 20
-    paddle.goto(new_x, paddle.ycor())
 
 screen.listen()
 
-screen.onkey(go_right, "Right")
-screen.onkey(go_left, "Left")
+
+
+screen.onkey(paddle.go_right, "Right")
+screen.onkey(paddle.go_left, "Left")
 
 game_is_on  = True
 
