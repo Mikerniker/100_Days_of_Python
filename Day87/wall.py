906ced131745b566e434a86289be9d7ab2bf550c
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-02-04 06:03:57 +0800

    changed wall class in wall.py

--- Day87/wall.py
+++ Day87/wall.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
 import random
 
-COLORS = ["orange", "green", "blue"]
 
+x = -360
+y = 0
+positions = []
 
 class Wall(Turtle):
 
     def __init__(self):
         super().__init__()
         self.all_bricks = []
+        self.build_wall()
