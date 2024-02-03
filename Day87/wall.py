39b81b349ca54c3c384ecbd620448c1df7a76c50
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-02-04 06:04:48 +0800

    added create_brick fxn to wallpy

--- Day87/wall.py
+++ Day87/wall.py
@@ -12,3 +12,18 @@ class Wall(Turtle):
         super().__init__()
         self.all_bricks = []
         self.build_wall()
+
+    def create_brick(self, position):
+        for pos in position:
+            new_brick = Turtle("square")
+            new_brick.shapesize(stretch_wid=1.2, stretch_len=4)
+            new_brick.penup()
+            new_brick.goto(pos[0], pos[1])
+       
+            if positions.index(position) < 2:
+                new_brick.color("blue")
+            elif 1 < positions.index(position) < 4:
+                new_brick.color("orange")
+            else:
+                new_brick.color("green")
+            self.all_bricks.append(new_brick)
