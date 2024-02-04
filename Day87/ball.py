cab824033c7820928e90ab0983c9acc78ff32557
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-02-05 01:20:47 +0800

    added bounce_x and reset_position fxns

--- Day87/ball.py
+++ Day87/ball.py
@@ -16,6 +16,12 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-
     def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, -40)
+        self.bounce_x()
