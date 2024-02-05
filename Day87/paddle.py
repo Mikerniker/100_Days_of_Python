4adc5a71d91096aa90e6a2a2546ddf168817f75e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-02-06 01:46:09 +0800

    modified paddle shapesize and movement

--- Day87/paddle.py
+++ Day87/paddle.py
@@ -5,17 +5,14 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid=1, stretch_len=8)
+        self.shapesize(stretch_wid=1, stretch_len=7)
         self.penup()
         self.goto(position)
-        # self.goto(0, -250)
-
-
+       
     def go_right(self):
-        new_x = self.xcor() + 20
+        new_x = self.xcor() + 50
         self.goto(new_x, self.ycor())
 
-
     def go_left(self):
-        new_x = self.xcor() - 20
+        new_x = self.xcor() - 50
         self.goto(new_x, self.ycor())
