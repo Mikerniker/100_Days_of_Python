d86f21bd19903334ec39dd03e2cb40c7e8e78f17
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-05-23 02:44:58 +0800

    install PyPDF2 and create get pdf text function

--- Day91/main.py
+++ Day91/main.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'add-secret-key-here'
+
+
+file = ""
+
+def get_pdf_text(pdf_file):
+    reader = PdfReader(pdf_file)
+    number_of_pages = len(reader.pages)
+    print(number_of_pages)
+    all_text = ""
+    for page_no in range(0, number_of_pages):
+        page = reader.pages[page_no]
+        text = page.extract_text()
+        all_text += text
+    return all_text
 
 
 @app.route("/", methods=["GET", "POST"])
