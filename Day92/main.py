e696d32dd0473c72e628d28bdd6f9e160953218a
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-06-08 04:15:07 +0800

    add get rgb function to main

--- Day92/main.py
+++ Day92/main.py
@@ -7,6 +7,24 @@ import pandas as pd
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'add-secret-key-here'
 
+def get_RGB():
+    img = Image.open(r"./static/assets/images/foodplatter.jpg")
+    convert_palette = img.convert("P", palette=Image.Palette.WEB)
+    palette = convert_palette.getpalette()   #get rgb
+    palette = [palette[3*n:3*n+3] for n in range(256)]
+    color_count = [(n, palette[m]) for n, m in convert_palette.getcolors()]
+
+    # Convert to a dataframe and get 10 most common
+    rgb_df = pd.DataFrame(color_count, columns=['count', "RGB"]).sort_values(by="count", ascending=False).iloc[0:10]
+    print(rgb_df)
+
+    # Convert df to RGB
+    RGB = pd.DataFrame(rgb_df['RGB'].to_list(), columns=['r', 'g', 'b'])
+    RGB['Total'] = RGB.r + RGB.g + RGB.b
+    RGB = RGB.sort_values(by=["Total"]).drop(['Total'], axis=1).reset_index().drop("index", axis=1)
+    print(f"RGB: {RGB}")
+    return RGB
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
