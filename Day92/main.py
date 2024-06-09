142f1ea7267ced4be8a5871d6a6a4e2f09997bbb
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-06-10 03:47:45 +0800

    fix get_RGB function and add parameters

--- Day92/main.py
+++ Day92/main.py
@@ -11,22 +11,22 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'add-secret-key-here'
 
-def get_RGB():
-    img = Image.open(r"./static/assets/images/foodplatter.jpg")
+
+def get_RGB(image_path, colors):
+    img = Image.open(image_path)
     convert_palette = img.convert("P", palette=Image.Palette.WEB)
-    palette = convert_palette.getpalette()   #get rgb
+    palette = convert_palette.getpalette()  #get rgb
     palette = [palette[3*n:3*n+3] for n in range(256)]
     color_count = [(n, palette[m]) for n, m in convert_palette.getcolors()]
 
     # Convert to a dataframe and get 10 most common
-    rgb_df = pd.DataFrame(color_count, columns=['count', "RGB"]).sort_values(by="count", ascending=False).iloc[0:10]
-    print(rgb_df)
-
+    rgb_df = pd.DataFrame(color_count, columns=['count', "RGB"]).sort_values(by="count", ascending=False).iloc[0:colors]
+   
     # Convert df to RGB
     RGB = pd.DataFrame(rgb_df['RGB'].to_list(), columns=['r', 'g', 'b'])
     RGB['Total'] = RGB.r + RGB.g + RGB.b
     RGB = RGB.sort_values(by=["Total"]).drop(['Total'], axis=1).reset_index().drop("index", axis=1)
-    print(f"RGB: {RGB}")
+   
     return RGB
 
 
