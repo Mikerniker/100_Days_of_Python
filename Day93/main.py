e8570d9415fc8f254246ff4d1780cc59378ee82f
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-06-17 04:15:28 +0800

    Try bs4

--- Day93/main.py
+++ Day93/main.py
@@ -1,6 +1,18 @@
-from selenium import webdriver
+from bs4 import BeatifulSoup
+import requests
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+#requests allows us to get hold of the data from a particular url
 
-driver = webdriver.Chrome(options=chrome_options)
+response = requests.get("https://flavorsofcacao.com/chocolate_database.html")
+chocolate_data = response.text
+
+soup = BeautifulSoup(chocolate_data, "html.parser")
+
+print(soup.title)
+
+# from selenium import webdriver
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("detach", True)
+
+# driver = webdriver.Chrome(options=chrome_options)
