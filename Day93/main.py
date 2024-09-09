bbbb0cec4b3ca8eab928e7909b70f6dc9b33d90d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-09-10 02:18:25 +0800

    add pandas import and access titles

--- Day93/main.py
+++ Day93/main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import pandas as pd
 
 response = requests.get("http://flavorsofcacao.com/database_w_REF.html")
 chocolate_data = response.text
@@ -11,19 +11,15 @@ soup = BeautifulSoup(chocolate_data, "html.parser")
 tables = soup.select(selector="#choco_database")
 print(tables)
 
-# print(tables)
-
-# table_titles = soup.findAll('table')[0].findAll('th')
-# # print(table_titles)
-
-# for title in table_titles:
-#     print(title.getText())
-
-
 
 table = soup.find_all('table')[0]
 
+#Get table titles
 table_titles = table.find_all('th')
+col_table_title = [title.text.strip() for title in table_titles]
+df = pd.DataFrame(columns=col_table_title)
+
+
 table_data = table.find_all('tr')
 
 column_title = [title.getText() for title in table_titles]
