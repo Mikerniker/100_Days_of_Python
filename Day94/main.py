906c4416d95073e6edb2bf5904ab5e773651c1c9
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-09-30 05:31:07 +0800

    initial pyautogui

--- Day94/main.py
+++ Day94/main.py
@@ -4,3 +4,9 @@ import webbrowser
 
 # Open the game URL
 webbrowser.open("https://elgoog.im/t-rex/")
+
+pyautogui.moveTo(100, 200)
+pyautogui.click()
+
+# Get the size of the primary monitor.
+screenWidth, screenHeight = pyautogui.size() 
