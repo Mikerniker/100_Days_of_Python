c8f31fc2c2c70ec03aa7f4b040d7d4bcc3cd7e49
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-12-06 02:37:44 +0800

    get width to right of dino

--- Day94/main.py
+++ Day94/main.py
@@ -7,6 +7,9 @@ top = 517
 width = 187
 height = 133
 
+# Define the Dino region and region to right of dino
+extra_width = 150  # width to the right of the dinosaur
+combined_region = (left, top, width + extra_width, height)
 
 # pyautogui.moveTo(100, 200)
 # pyautogui.click()
