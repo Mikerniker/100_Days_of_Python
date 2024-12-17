5aa4db6c65c56c837fa1ee532fe2c2ec83d09080
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-12-18 04:04:06 +0800

    add kivy imports

--- Day95/main.py
+++ Day95/main.py
@@ -1,7 +1,13 @@
-import time
-from turtle import Screen
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+
+# import time
+# from turtle import Screen
 
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.tracer(0)
