1cb04ee9622c4ed35ead090799e1103f16c0627b
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-12-22 05:53:48 +0800

    modify update

--- Day95/main.py
+++ Day95/main.py
@@ -32,10 +32,10 @@ class SpaceGame(Widget):
                 self.add_widget(alien)
                 self.aliens.append(alien)
 
-    # def update(self, dt):
-    #     # self.alien.move()
-    #     for alien in self.aliens:
-    #         alien.move()
+    def update(self, dt):
+        # Move all aliens left
+        for alien in self.aliens:
+            alien.x += self.alien_velocity_x
 
 class SpaceApp(App):
     def build(self):
