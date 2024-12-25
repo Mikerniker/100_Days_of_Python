0d3862eacca63651a48ed9c5d96a92f805be5e07
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-12-26 00:59:48 +0800

    add move fxn for alien

--- Day95/main.py
+++ Day95/main.py
@@ -61,7 +61,9 @@ class Alien(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
-
+    
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 if __name__ == '__main__':
