7ca607ef35a7bb1ba6ced1fa3ccc4d2014d61fa2
Mikerniker <watkins.michelle@gmail.com>
2025-05-21 02:45:21 +0800

    add fxns to move spaceship

--- Day95/spaceship.py
+++ Day95/spaceship.py
@@ -11,3 +11,9 @@ class Spaceship(Turtle):
         self.penup()
         self.speed("fastest")
         self.goto(SHIP_COORDINATES)
+
+    def move_left(self):
+        self.backward(MOVE_DISTANCE)
+
+    def move_right(self):
+        self.forward(MOVE_DISTANCE)
