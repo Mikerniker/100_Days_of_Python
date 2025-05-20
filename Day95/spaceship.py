62423cd0c2a5403a2d8cb8d577cf61c51d6b299c
Mikerniker <watkins.michelle@gmail.com>
2025-05-21 02:44:58 +0800

    create spaceship

---
+++ Day95/spaceship.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+SHIP_COORDINATES = (0, -170)
+MOVE_DISTANCE = 25
+
+class Spaceship(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("spaceship.gif")
+        self.color("white")
+        self.penup()
+        self.speed("fastest")
+        self.goto(SHIP_COORDINATES)
