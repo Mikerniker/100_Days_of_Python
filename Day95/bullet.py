90cabeadcb7b5c30e8f5d0a53371237718230ca4
Mikerniker <watkins.michelle@gmail.com>
2025-05-22 03:35:29 +0800

    add create_bullet fxn

--- Day95/bullet.py
+++ Day95/bullet.py
@@ -4,10 +4,14 @@ MOVE_DISTANCE = 20
 
 
 class Bullet(Turtle):
-   def __init__(self):
+    def __init__(self):
        super().__init__()
        self.shape("circle")
        self.setheading(90)
        self.penup()
        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
        self.color("white")
+
+
+    def create_bullet(self, position):
+       self.goto(position)
