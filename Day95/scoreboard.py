93762899343ac8356cddbc5dad18e6b6dc68d703
Mikerniker <watkins.michelle@gmail.com>
2025-05-23 03:37:41 +0800

    simplify scoreboard init

--- Day95/scoreboard.py
+++ Day95/scoreboard.py
@@ -7,19 +7,10 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.lives = 3
-        self.show_score = self.display_text((-130, 175))
-        self.show_lives = self.display_text((130, 175))
-        self.game_is_over = self.display_text((0, 0))
+        self.color("white")
+        self.hideturtle()
+        self.penup()
         self.update_scoreboard()
-        self.update_lives()
-
-    def display_text(self, position):
-        text = Turtle()
-        text.hideturtle()
-        text.penup()
-        text.color("white")
-        text.goto(position)
-        return text
 
     def update_scoreboard(self):
         self.show_score.clear()
