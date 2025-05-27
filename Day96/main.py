c6cf8ce213395e4f7aa63b19b547119548861fec
Mikerniker <watkins.michelle@gmail.com>
2025-05-28 03:00:13 +0800

    create initial dataframe

--- Day96/main.py
+++ Day96/main.py
@@ -9,3 +9,19 @@ response.raise_for_status()
 data = response.json()
 print(data["Data"])
 all_data = data["Data"]
+
+
+st.title("Top Crypto Gainers & Losers (24h)")
+
+df = pd.DataFrame([
+    {
+        'Symbol': item['CoinInfo'].get('Name'),
+        'Full Name': item['CoinInfo'].get('FullName'),
+        'Image URL': "https://www.cryptocompare.com" + item['CoinInfo'].get('ImageUrl', ''),
+        'Supply': item['ConversionInfo'].get('Supply'),
+        'Total Volume 24H': item['ConversionInfo'].get('TotalVolume24H'),
+    }
+    for item in all_data
+])
+
+
