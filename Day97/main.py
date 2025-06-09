66abf83f87258b8aa139301c753bc2c60114b661
Mikerniker <watkins.michelle@gmail.com>
2025-06-10 03:32:11 +0800

    add initial moralis code

--- Day97/main.py
+++ Day97/main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+# from flask import Flask
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from moralis import sol_api
+
+
+api_key = "MY_MORALIS_API_KEY"
+CHAIN = "solana"
+COLLECTION_ADDRESS ="*********************"
