7358ed23a4ff2ddee1e6bf3e6b78c476c2090782
Mikerniker <watkins.michelle@gmail.com>
2025-06-11 02:55:34 +0800

    add imports

--- Day97/main.py
+++ Day97/main.py
@@ -1,34 +1,36 @@
-# from flask import Flask
-
-
-# app = Flask(__name__)
-
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
+from flask import Flask
 from moralis import sol_api
+import random
+import datetime
+
+app = Flask(__name__)
 
 
-api_key = "MY_MORALIS_API_KEY"
-CHAIN = "solana"
-COLLECTION_ADDRESS ="*********************"
 
-params = {
-  "nft_metadata": True,
-  "media_items": False,
-  "exclude_spam": False,
-  "network": "mainnet",
-  "address": COLLECTION_ADDRESS
-}
 
-result = sol_api.account.get_nfts(
-  api_key=api_key,
-  params=params,
-)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+# api_key = "MY_MORALIS_API_KEY"
+# CHAIN = "solana"
+# COLLECTION_ADDRESS ="*********************"
+
+# params = {
+#   "nft_metadata": True,
+#   "media_items": False,
+#   "exclude_spam": False,
+#   "network": "mainnet",
+#   "address": COLLECTION_ADDRESS
+# }
+
+# result = sol_api.account.get_nfts(
+#   api_key=api_key,
+#   params=params,
+# )
 
 # print(result)
