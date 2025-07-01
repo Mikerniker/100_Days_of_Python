2461e35a15d59c7d02ae50c243b49bc51cc0310d
Mikerniker <watkins.michelle@gmail.com>
2025-07-02 03:19:37 +0800

    add initial net work fxn

--- Day98/utils.py
+++ Day98/utils.py
@@ -4,6 +4,24 @@ from moralis import evm_api
 # MORALIS_API_KEY = os.getenv("MORALIS_API_KEY")
 MORALIS_API_KEY="api_key"
 
+
+def get_net_worth():
+    params = {
+        "exclude_spam": True,
+        "exclude_unverified_contracts": true,
+        "max_token_inactivity": 1,
+        "min_pair_side_liquidity_usd": 1000,
+        "address": ""
+    }
+
+    result = evm_api.wallets.get_wallet_net_worth(
+        api_key=MORALIS_API_KEY,
+        params=params,
+    )
+
+    return result
+
+
 def get_eth_wallet_balance(addresses):
     pass
 
