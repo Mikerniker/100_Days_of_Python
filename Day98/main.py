97932e149e927829143e95f706c0f72092285eba
Mikerniker <watkins.michelle@gmail.com>
2025-07-05 02:21:43 +0800

    add initial table display

--- Day98/main.py
+++ Day98/main.py
@@ -4,3 +4,20 @@ from utils import get_token_prices
 
 # Streamlit UI
 st.title("Crypto Price Alert")
+
+
+def display_table():
+    # st.markdown(f"## {title}")
+    header_cols = st.columns([1, 2, 3, 2, 2])
+    with header_cols[0]:
+        st.markdown("**Logo**")
+    with header_cols[1]:
+        st.markdown("**Symbol**")
+    with header_cols[2]:
+        st.markdown("**Full Name**")
+    with header_cols[3]:
+        st.markdown("**Price**")
+    with header_cols[4]:
+        st.markdown("**Percent Change**")
+
+   
