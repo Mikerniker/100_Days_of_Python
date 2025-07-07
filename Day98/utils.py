6d75e8cff9720a749979868a92db05393d2a0198
Mikerniker <watkins.michelle@gmail.com>
2025-07-08 02:14:05 +0800

    add initial check_price

--- Day98/utils.py
+++ Day98/utils.py
@@ -5,21 +5,21 @@ from moralis import evm_api
 MORALIS_API_KEY="api_key"
 
 
-def get_net_worth():
-    params = {
-        "exclude_spam": True,
-        "exclude_unverified_contracts": true,
-        "max_token_inactivity": 1,
-        "min_pair_side_liquidity_usd": 1000,
-        "address": ""
-    }
+# def get_net_worth():
+#     params = {
+#         "exclude_spam": True,
+#         "exclude_unverified_contracts": true,
+#         "max_token_inactivity": 1,
+#         "min_pair_side_liquidity_usd": 1000,
+#         "address": ""
+#     }
 
-    result = evm_api.wallets.get_wallet_net_worth(
-        api_key=MORALIS_API_KEY,
-        params=params,
-    )
+#     result = evm_api.wallets.get_wallet_net_worth(
+#         api_key=MORALIS_API_KEY,
+#         params=params,
+#     )
 
-    return result
+#     return result
 
 
 def get_token_prices():
@@ -44,12 +44,7 @@ def get_token_prices():
    
    return result
 
-# result = get_token_prices()
-# # print(result)
-# for i in result:
-#     # print(type(i))
-#     print(i['usdPrice'])
-#     print(i['tokenName'])
-#     print(i['tokenSymbol'])
-#     print(i['tokenLogo'])
+def check_price(change):
+   
+
 
