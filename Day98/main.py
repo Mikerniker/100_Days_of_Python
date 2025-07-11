5ccc806f9e43af2680fc3ed6db468b08e082b42a
Mikerniker <watkins.michelle@gmail.com>
2025-07-12 03:40:42 +0800

    add streamlit view

--- Day98/main.py
+++ Day98/main.py
@@ -1,24 +1,13 @@
 import streamlit as st
 from utils import get_btc_details
 
+
+result = get_btc_details()
+
 # Streamlit UI
-st.title("Crypto Price Alert")
 
+st.title("BITCOIN Price Alert")
 
-def display_table():
-    # st.markdown(f"## {title}")
-    header_cols = st.columns([1, 2, 3, 2, 2])
-    with header_cols[0]:
-        st.markdown("**Logo**")
-    with header_cols[1]:
-        st.markdown("**Symbol**")
-    with header_cols[2]:
-        st.markdown("**Full Name**")
-    with header_cols[3]:
-        st.markdown("**Price**")
-    with header_cols[4]:
-        st.markdown("**Percent Change**")
-
-    result = get_token_prices()
-    # print(result)
-   
+st.image(result.get("logo"), width=100)
+st.markdown(f"## BTC Price: $ {result.get('current_price'):.2f}")
+st.markdown(f"## Price Change {result.get('price_change_24h')}%")
