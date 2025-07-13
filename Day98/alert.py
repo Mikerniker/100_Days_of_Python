a29e45148145fd210db5077badf5a2c1f6bb285f
Mikerniker <watkins.michelle@gmail.com>
2025-07-14 03:28:26 +0800

    add basic config

--- Day98/alert.py
+++ Day98/alert.py
@@ -3,3 +3,13 @@ import logging
 from telegram import Update
 from telegram.ext import  filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 from main import get_btc_details
+
+
+TOKEN ="my_token"
+MY_ID = "my_id"
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
