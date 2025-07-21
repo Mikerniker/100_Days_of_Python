7ea457c9da703efb45e449c851da2d99a9d6a936
Mikerniker <watkins.michelle@gmail.com>
2025-07-22 03:14:20 +0800

    add welcome_user fxn

--- Day98/alert.py
+++ Day98/alert.py
@@ -31,19 +31,23 @@ async def get_user():
         else:
             return None
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hi, I'm a BTC alert bot! To get started type /btc_price to get the current BTC price"
-    )
 
-async def btc_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    data = get_btc_details()
-    price = data['current_price']
+async def welcome_user():
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=f"BTC price is ${price:,.2f}"
-    )
+
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(
+#         chat_id=update.effective_chat.id,
+#         text="Hi, I'm a BTC alert bot! To get started type /btc_price to get the current BTC price"
+#     )
+
+# async def btc_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     data = get_btc_details()
+#     price = data['current_price']
+
+#     await context.bot.send_message(
+#         chat_id=update.effective_chat.id, text=f"BTC price is ${price:,.2f}"
+#     )
 
 
 
