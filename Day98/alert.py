8ef4ebd5ad05b8817cd8eb17786bcc2c376b43f0
Mikerniker <watkins.michelle@gmail.com>
2025-07-26 03:38:50 +0800

    set higher logging level for httpx

--- Day98/alert.py
+++ Day98/alert.py
@@ -20,7 +20,9 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
+logger = logging.getLogger(__name__)
 
 async def get_user():
     bot = telegram.Bot(TOKEN)
