afc77ba15e793400d18fb3423c780d8f16cd0ebd
Mik Watkins <watkins.michelle@gmail.com>
2026-01-08 02:56:02 +0800

    add initial data cleaning section

--- Day99/main.py
+++ Day99/main.py
@@ -25,15 +25,19 @@ if uploaded_file:
         st.write("**What is the shape of `df_data`?**")
         st.write(df.shape)
 
-    st.write("**How many rows and columns does it have?**")
+        st.write("**How many rows and columns does it have?**")
 
-    st.write("It has 4324 rows and 9 columns")
+        st.write("It has 4324 rows and 9 columns")
 
-    st.write("**What are the column names?**")
-    st.write(df.columns)
+        st.write("**What are the column names?**")
+        st.write(", ".join(df.columns))
 
-    st.write("**Are there any NaN values or duplicates?**")
-    # st.write(df.isna())
+        st.write("**Are there any NaN values or duplicates?**")
+        # st.write(df.isna())
 
-    total_nans = df.isna().sum().sum()
-    st.write(f"There are {total_nans} missing (NaN) values in the entire DataFrame")
+        total_nans = df.isna().sum().sum()
+        st.write(f"There are {total_nans} missing (NaN) values in the entire DataFrame")
+    
+    st.subheader("**Data Cleaning - Check for Missing Values and Duplicates**")
+  
+    with st.expander("What Data is Missing"):
