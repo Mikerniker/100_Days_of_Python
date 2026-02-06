eaaf0292eb529a552c10ba66d39fb6a4018bb305
Mik Watkins <watkins.michelle@gmail.com>
2026-02-07 05:38:16 +0800

    read csvs

--- Day100/main.py
+++ Day100/main.py
@@ -38,3 +38,11 @@ pd.options.display.float_format = '{:,.2f}'.format
 
 st.markdown("<h1 style='text-align: center;'>Deaths by Police in the United States</h1>", unsafe_allow_html=True)
 
+
+# LOAD DATA
+# ============================================================================
+df_hh_income = pd.read_csv('Median_Household_Income_2015.csv', encoding="windows-1252")
+df_pct_poverty = pd.read_csv('Pct_People_Below_Poverty_Level.csv', encoding="windows-1252")
+df_pct_completed_hs = pd.read_csv('Pct_Over_25_Completed_High_School.csv', encoding="windows-1252")
+df_share_race_city = pd.read_csv('Share_of_Race_By_City.csv', encoding="windows-1252")
+df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
