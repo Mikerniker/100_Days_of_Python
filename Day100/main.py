70f73ef32e2c95655c13e75e2bf74c0bb2c0df56
Mik Watkins <watkins.michelle@gmail.com>
2026-02-07 05:39:48 +0800

    add all_data

--- Day100/main.py
+++ Day100/main.py
@@ -39,10 +39,13 @@ pd.options.display.float_format = '{:,.2f}'.format
 st.markdown("<h1 style='text-align: center;'>Deaths by Police in the United States</h1>", unsafe_allow_html=True)
 
 
-# LOAD DATA
-# ============================================================================
+# LOAD DATA  ============================================================================
 df_hh_income = pd.read_csv('Median_Household_Income_2015.csv', encoding="windows-1252")
 df_pct_poverty = pd.read_csv('Pct_People_Below_Poverty_Level.csv', encoding="windows-1252")
 df_pct_completed_hs = pd.read_csv('Pct_Over_25_Completed_High_School.csv', encoding="windows-1252")
 df_share_race_city = pd.read_csv('Share_of_Race_By_City.csv', encoding="windows-1252")
 df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
+
+
+all_data = [df_hh_income, df_pct_poverty, df_pct_completed_hs,
+            df_share_race_city, df_fatalities]
