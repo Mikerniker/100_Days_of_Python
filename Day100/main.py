63ab1e3802cba384dde2cd4794a62e5450fc8225
Mik Watkins <watkins.michelle@gmail.com>
2026-02-07 05:37:20 +0800

    add config

--- Day100/main.py
+++ Day100/main.py
@@ -4,3 +4,13 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+# CONFIGURATION ============================================================================
+st.set_page_config(
+    page_title="Deaths By Police",
+    page_icon="🚓",
+    layout="wide",
+)
+pd.options.display.float_format = '{:,.2f}'.format
+
