962a6453ecbf618f94a1a15e876fa6c0bdd061f6
Mik Watkins <watkins.michelle@gmail.com>
2026-02-08 05:05:59 +0800

    fix csv filepath

--- Day99/main.py
+++ Day99/main.py
@@ -51,7 +51,7 @@ st.markdown("<h1 style='text-align: center;'>The Space Race 1957-2020</h1>", uns
 st.markdown("<h3 style='text-align: center;'>Submission for Day 99 of Angela Yu's Python Course</h3>", unsafe_allow_html=True)
 
 
-uploaded_file = "mission_launches.csv"
+uploaded_file = BASE_DIR / "mission_launches.csv"
 
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
