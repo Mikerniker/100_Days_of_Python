d1f27e9d314182aabdb5e2b5af983d1772b7838d
Mik Watkins <watkins.michelle@gmail.com>
2026-02-16 04:09:38 +0800

    add  initial age page

---
+++ Day100/assets/Age.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
+
+race_map = {
+    'W': 'White',
+    'B': 'Black',
+    'A': 'Asian',
+    'H': 'Hispanic',
+    'N': 'Native American',
+    'O': 'Other'
+}
+
+st.title("How Old Were the People Killed?")
+# How Old Were the People Killed?
+# Work out what percentage of people killed were under 25 years old.
+# Create a histogram and KDE plot that shows the distribution of ages of the people killed by police.
+# Create a seperate KDE plot for each race. Is there a difference between the distributions?
+# Ensure age is numeric
