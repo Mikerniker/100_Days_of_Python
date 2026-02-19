12df2c00ee293a37aff34e5bd3b8b67746882531
Mik Watkins <watkins.michelle@gmail.com>
2026-02-20 04:31:33 +0800

    add race_map

--- Day100/pages/City.py
+++ Day100/pages/City.py
@@ -6,3 +6,15 @@ import seaborn as sns
 
 
 df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
+
+race_map = {
+    'W': 'White',
+    'B': 'Black',
+    'A': 'Asian',
+    'H': 'Hispanic',
+    'N': 'Native American',
+    'O': 'Other'
+}
+# In Which Cities Do the Most Police Killings Take Place?
+# Create a chart ranking the top 10 cities with the most police killings.
+# Which cities are the most dangerous?
