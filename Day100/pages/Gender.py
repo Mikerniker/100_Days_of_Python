c13cf39f58979d494628c4d4c20a20cd2e9a1e55
Mik Watkins <watkins.michelle@gmail.com>
2026-02-26 04:58:56 +0800

    add inital to do and cols

--- Day100/pages/Gender.py
+++ Day100/pages/Gender.py
@@ -3,3 +3,10 @@ import pandas as pd
 import plotly.express as px
 
 df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
+
+# TO DO
+# Create a Chart Comparing the Total Number of Deaths of Men and Women
+# Use `df_fatalities` to illustrate how many more men are killed compared to women.
+
+col1, col2 = st.columns(2)
+with col1:
