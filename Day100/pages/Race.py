5fd6dd1c8575fdbd1bc38e1b178621ad9608def5
Mik Watkins <watkins.michelle@gmail.com>
2026-03-01 05:34:45 +0800

    add dbase

--- Day100/pages/Race.py
+++ Day100/pages/Race.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
+
+df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
+df_share_race_city = pd.read_csv('Share_of_Race_By_City.csv', encoding="windows-1252")
