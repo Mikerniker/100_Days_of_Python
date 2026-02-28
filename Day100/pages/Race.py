184dd51d8d6f1c77365a66b7186096a60ca0d3a9
Mik Watkins <watkins.michelle@gmail.com>
2026-03-01 05:35:10 +0800

    add race_fatalities and race_map

--- Day100/pages/Race.py
+++ Day100/pages/Race.py
@@ -5,3 +5,14 @@ import matplotlib.pyplot as plt
 
 df_fatalities = pd.read_csv('Deaths_by_Police_US.csv', encoding="windows-1252")
 df_share_race_city = pd.read_csv('Share_of_Race_By_City.csv', encoding="windows-1252")
+
+race_fatalities = df_fatalities['race'].value_counts()
+
+race_map = {
+    'W': 'White',
+    'B': 'Black',
+    'A': 'Asian',
+    'H': 'Hispanic',
+    'N': 'Native American',
+    'O': 'Other'
+}
